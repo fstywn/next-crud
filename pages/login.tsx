23b7ecd7c1677142430c936da84385d4a67c6019
fstywn <fjrstywn22@gmail.com>
2023-07-01 22:17:17 +0700

    add google icon

--- pages/login.tsx
+++ pages/login.tsx
@@ -3,6 +3,7 @@ import { signIn } from "next-auth/react";
 import { NextApiRequest, NextApiResponse } from "next";
 import { getServerSession } from "next-auth";
 import { authOptions } from "./api/auth/[...nextauth]";
+import GoogleIcon from "@/components/google";
 
 export default function Login() {
   return (
@@ -16,6 +17,7 @@ export default function Login() {
           onClick={async () => await signIn("google")}
           className="bg-white/5 py-2 px-8 rounded flex items-center gap-2 mx-auto"
         >
+          <GoogleIcon />
           Login with Google &rarr;
         </button>
       </main>
